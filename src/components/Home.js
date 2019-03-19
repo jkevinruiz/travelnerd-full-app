7b8583a029358682b16eb3c663e107bfdff22e9a
Lee Palisoc <leepalisoc@me.com>
2019-03-19 15:03:27 -0600

    Revert "Updating our base app using Ben's"

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,24 +3,28 @@ import './Home.css';
 import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
-	
-	render() {
-		return (
-			<div className="banner">
-				<div>
-					<h1>Travel Photos</h1>
-					<p>
-						<Link to='/browse'>
-							<button>Browse</button>
-						</Link>
-						<Link to='/about'>
-							<button>About</button>
-						</Link>
-					</p>
-				</div>
-			</div>
-		);
-	}
+
+    /**
+     * Renders/Displays website elements.
+     */
+    render() {
+        return (
+            <div className="banner">
+                <div>
+                    <h1>Travel Nerd</h1>
+                    <h3>Browse & Share</h3>
+                    <p>
+                        <Link to='/browse'>
+                            <button>Browse</button>
+                        </Link>
+                        <Link to='/about'>
+                            <button>About</button>
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Home;
