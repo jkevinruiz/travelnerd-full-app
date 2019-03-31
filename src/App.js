d75341f467433f344296804bace60881c7a2d332
leepalisoc <leepalisoc@me.com>
2019-03-30 21:33:23 -0600

    Revert "Uhh..."
    This reverts commit 0a9674b9d06ff547d5f102ef7804a2e747b9ec2d.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Home from './components/Home.js';
 import About from './components/About.js';
 import Favourites from './components/Favourites.js';
 
-
+	
 function urlToPromise(url) {
 	return new Promise(function(resolve, reject) {
 		JSZipUtils.getBinaryContent(url, function (err, data) {
@@ -22,29 +22,29 @@ function urlToPromise(url) {
 		});
 	});
 }
-
+	
 
 class App extends Component {
-
+	
 	constructor(props) {
 		super(props);
 		this.state = { photos: [], favourites: [] };
 	}
 
 	downloadFaves = () => {
-
+		
 		const favePhotos = this.state.photos.filter((p) => this.state.favourites.includes(p.id));
 		const imgURL = 'https://storage.googleapis.com/funwebdev-3rd-travel/large/';
 		const proxyURL = 'https://cors-anywhere.herokuapp.com/';
-
+		
 		var zip = new JSZip();
-
+		
 		favePhotos.forEach((p) => {
 			let filePath = proxyURL + imgURL + p.path;
 			zip.file(`Travel Image ${p.id}.jpg`, urlToPromise(filePath), {binary:true});
 			console.log(`Zipped Travel Image ${p.id}.jpg`);
 		});
-
+		
 		zip.generateAsync({type:"blob"})
 			.then(function callback(blob) {
 
@@ -54,36 +54,36 @@ class App extends Component {
 	}
 
 	addToFavourites = (id) => {
-
+		
 		if (!this.state.favourites.includes(id)){
 			let prevState = this.state.favourites;
 			this.setState( {favourites: [...prevState, id]}, this.saveFaves );
 		}
-
-
+		
+		
 	}
-
+	
 	saveFaves = () => {
 		localStorage.setItem('favourites', JSON.stringify(this.state.favourites));
 	}
-
+	
 	removeFromFavourites = (id) => {
-
+		
 		let oldFaves = [...this.state.favourites];
 		let index = oldFaves.indexOf(id);
 		oldFaves.splice(index, 1);
 		this.setState( {favourites: oldFaves}, this.saveFaves );
-
+		
 	}
-
+	
 	updatePhoto = (id, photo) => {
-
+		
 		// Create deep clone of photo array from state using lodash
 		const copyPhotos = cloneDeep(this.state.photos);
-
+		
 		// Find photo to update in cloned array
 		const photoToReplace = copyPhotos.find( p => p.id === id);
-
+		
 		// replace photo fields with edited values
 		photoToReplace.title = photo.title;
 		photoToReplace.description = photo.description;
@@ -91,22 +91,22 @@ class App extends Component {
 		photoToReplace.country = photo.country;
 		photoToReplace.latitude = photo.latitude;
 		photoToReplace.longitude = photo.longitude;
-
+		
 		//update state
 		this.setState( {photos: copyPhotos});
 	}
-
+	
 	deletePhoto = (id) => {
 		let oldPhotos = [...this.state.photos];
 		const index = oldPhotos.findIndex(p => p.id ===id)
 		oldPhotos.splice(index, 1);
 		this.setState( {photos: oldPhotos} );
 	}
-
+	
 	logPhotos = () => {
 		console.log(this.state.photos);
 	}
-
+	
 	async componentDidMount() {
 		if (localStorage.getItem('favourites') != null) {
 			this.setState( {favourites: JSON.parse(localStorage.getItem('favourites'))} );
@@ -121,7 +121,7 @@ class App extends Component {
 			console.error(error);
 		}
 	}
-
+	
 	render() {
 		return (
 			<div>
@@ -129,8 +129,8 @@ class App extends Component {
 				<Route path='/' exact component={Home} />
 				<Route path='/home' exact component={Home} />
 				<Route path='/about' exact component={About} />
-				<Route path='/browse' exact
-					render={ (props) =>
+				<Route path='/browse' exact 
+					render={ (props) => 
 						<div>
 							<Favourites
 								photos={this.state.photos}
