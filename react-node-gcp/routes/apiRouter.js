f62724a490575927957d9969e6d4d63472b136a6
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2019-04-02 21:28:14 -0600

    Added images/id route
    Updated apirouter file to include id image route.

--- react-node-gcp/routes/apiRouter.js
+++ react-node-gcp/routes/apiRouter.js
@@ -6,7 +6,17 @@ const router = express.Router();
 router.get('/images', (req, resp) => {
     ImageModel.find({}, (err, data) => {
         if (err) {
-            resp.json({ Error: err});
+            resp.json({ Error: 'Images not found'});
+        } else {
+            resp.json(data);
+        }
+    });
+});
+
+router.get('/images/:id', (req, resp) => {
+    ImageModel.find({id: req.params.id}, (err, data) => {
+        if (err) {
+            resp.json({Error: 'Image not found'});
         } else {
             console.log(data);
             resp.json(data);
