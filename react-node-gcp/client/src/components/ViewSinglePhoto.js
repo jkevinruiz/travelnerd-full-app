0eac5537b3257319a8b9f821052610ed4b1f5881
rmcco97 <rmcco668@mtroyal.ca>
2019-04-05 16:10:56 -0600

    Added EXIF information to ViewSinglePhoto - Ross

--- react-node-gcp/client/src/components/ViewSinglePhoto.js
+++ react-node-gcp/client/src/components/ViewSinglePhoto.js
@@ -22,10 +22,11 @@ class ViewSinglePhoto extends React.Component {
                                 <p>{photo.location.city}, {photo.location.country}</p>
                                 <button onClick={this.handleEdit}>Edit</button>
                                 <button onClick={this.handleMap}>Map</button>
-                                <span>
-                                    <p>EXIF Data</p>
-                                    <p>`Make: ${photo.exif.make}`</p>
-                                </span>
+                                {this.handleEXIFDate(photo)}
+                                {/* <span>
+                                    <p>Shot with: {photo.exif.make}, {photo.exif.model}</p>
+                                    <p>Exposure: {photo.exif.exposure_time}</p><p>Aperature: {photo.exif.aperature}</p>
+                                </span> */}
                             </div>
                         </div>
                     </article>
@@ -56,6 +57,41 @@ class ViewSinglePhoto extends React.Component {
      */
     handleMap = () => {
         this.props.changeRenderView("map");
+    }
+
+    handleEXIFDate = (photo) => {
+        var make = 'Not available';
+        var model = 'Not available';
+        var exposure = 'Not available';
+        var aperature = 'Not available';
+        var focalLength = 'Not available';
+        var iso = 'Not available';
+        
+
+        if(photo.exif.make != undefined) {
+            make = photo.exif.make; 
+        }
+        if(photo.exif.model != undefined) {
+            model = photo.exif.model;
+        }
+        if(photo.exif.exposure_time != undefined) {
+            exposure = photo.exif.exposure_time;
+        }
+        if(photo.exif.aperature != undefined) {
+            aperature = photo.exif.aperature;
+        }
+        if(photo.exif.focal_length != undefined) {
+            iso = photo.exif.iso;
+        }
+
+        return(
+            <div className="exifInfo"> 
+                <p><strong>Shot with:</strong> {make}, {model}</p>
+                <p><strong>Exposure:</strong> {exposure}, <strong>Aperature:</strong> {aperature}</p>
+                <p><strong>Focal Length:</strong> {focalLength}, <strong>ISO:</strong> {iso}</p>
+            </div>
+        );
+        
     }
 
 
