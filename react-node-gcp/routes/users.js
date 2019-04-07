5443a061f931bc1178041d7cbb7392e9e997315c
Ben Carr <ben.j.g.carr@gmail.com>
2019-04-06 23:32:44 -0600

    Removed bcrypt and added bcryptjs

--- react-node-gcp/routes/users.js
+++ react-node-gcp/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const passport = require("../config/auth")
 const uuid = require("uuid/v4");
 
